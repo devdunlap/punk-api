e0739a483db2ca192fefa7fd048ca317e021f792
devdunlap <108426486+devdunlap@users.noreply.github.com>
2023-04-09 17:21:47 -0500

    commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import SearchBox from './SearchBox'
+
+
+const Navbar = () => {
+ 
+ 
+ 
+    return (
+    
+    <div>
+    <SearchBox/>
+    <div>Classic Range</div>
+    <div>Special Range</div>
+    <div>Acidic/ph</div>
+    </div>
+    )
+}
+
+
+export default Navbar
